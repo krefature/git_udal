fd55cb9c37fad2e7f59e7feccc60f42103fccb73
User Name <johndoe@example.com>
2021-01-28 17:11:22 +0600

    commit message

--- 4.py
+++ 4.py
@@ -1,4 +1,6 @@
 f = open('Perepis.txt', 'r+')
+u1=int(input())
+u2=int(input())
 summa=0
 with open('Perepis.txt') as f:
     for i in f:
@@ -7,4 +9,8 @@ with open('Perepis.txt') as f:
             print(i[:-13])
             summa+=1
     print(summa)
+    for i in f:
+        chislo=int(i[-5:-1])
+        if chislo<u2 and chislo>u1:
+            print(i[:-13])
 f.close()
